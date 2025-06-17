57266e1e6593af6996464e6dfb66a73598b02787
Adalcar2021 <adalcar-1@hotmail.com>
2025-06-16 23:06:54 -0500

    informacion

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const buttun =document.getElementById('changetextButton');
+    buttun.addEventListener('click',function(){
+        const Intersection=document.getElementById('intro')
+    introSection.innrHTML="<p> El texto ha cambiado de spues de hacer clic en boton!!<p>";
+});
+});
