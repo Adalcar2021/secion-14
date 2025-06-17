e0d5393dcfa4db713a12f0949edc584a53419d40
Adalcar2021 <adalcar-1@hotmail.com>
2025-06-16 23:21:22 -0500

    nuevas secciones

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function(){
     const buttun =document.getElementById('changetextButton');
     buttun.addEventListener('click',function(){
         const Intersection=document.getElementById('intro')
-    introSection.innrHTML="<p> El texto ha cambiado de spues de hacer clic en boton!!<p>";
+    introSection.innrHTML='<p> El texto ha cambiado de spues de hacer clic en boton || <p>';
 });
 });
